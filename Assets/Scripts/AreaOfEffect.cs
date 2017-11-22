cc807da4fbebc8d76a510296a0c8224ac024cebe
Liams_laptop\Liam <liamodc@gmail.com>
2017-11-22 22:23:58 +0000

    Added scripts for Zeus AOE ability to fire Convert events to citizens if in radius, and destroy the AOE object on end animation.

---
+++ Assets/Scripts/AreaOfEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaOfEffect : MonoBehaviour 
+{
+
+	void OnEnable() {
+		CircleCollider2D collider = GetComponent<CircleCollider2D>();
+		var overlapped = Physics2D.OverlapCircleAll(collider.bounds.center, collider.radius);
+
+		for (int i = 0; i < overlapped.Length; i++) 
+		{
+			if (overlapped[i].name.Contains("Citizen")) 
+			{
+				// Convert to Zeus
+				overlapped[i].gameObject.SendMessage("OnConvert", 1, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
