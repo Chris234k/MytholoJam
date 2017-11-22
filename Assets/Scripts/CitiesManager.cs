6120de9ed820de9f1e8ee5e0b583438aab32d0b6
Liams_laptop\Liam <liamodc@gmail.com>
2017-11-22 22:22:31 +0000

    CitiesManager now handles unlocking and triggering of god abilities

--- Assets/Scripts/CitiesManager.cs
+++ Assets/Scripts/CitiesManager.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class CitiesManager : MonoBehaviour 
 {
-	public City[] cities;
+	public City zeusCity, poseidonCity, athenaCity;
+	public GameObject zeusAOE;
 
-	void Start() 
+	bool zeusAbilityUnlocked, poseidonAbilityUnlocked, athenaAbilityUnlocked;
+
+	void Update() 
 	{
-		
+		if (Input.GetKeyDown("z") && zeusAbilityUnlocked)
+    	{
+    		GameObject.Instantiate(zeusAOE);
+    	}
+	}
+
+	void UnlockAbility(City city)
+	{
+		switch (city.alignment)
+		{
+			case Alignment.Zeus: 
+				zeusAbilityUnlocked = true; 
+				break;
+			case Alignment.Poseidon: 
+				poseidonAbilityUnlocked = true;
+				break;
+			case Alignment.Athena: 
+				athenaAbilityUnlocked = true;
+				break;
+		}
 	}
 }
