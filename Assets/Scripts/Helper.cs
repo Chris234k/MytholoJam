73f058d403b21eb4cb1417015d3ba49f644b4dcb
Liams_laptop\Liam <liamodc@gmail.com>
2017-11-22 18:43:56 +0000

    Genericised Shuffle Helper method to use any GameObjects

--- Assets/Scripts/Helper.cs
+++ Assets/Scripts/Helper.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Helper
 {
-	public static City[] ShuffleGameObjects(City[] array)
+	public static GameObject[] ShuffleGameObjects(GameObject[] array)
 	{
 		// Fisher-Yates shuffle
 		for (int i = 0; i < array.Length; i++)
 		{
-			City temp = array[i];
+			GameObject temp = array[i];
 			int index = Random.Range(i, array.Length);
 			array[i] = array[index];
 			array[index] = temp;
