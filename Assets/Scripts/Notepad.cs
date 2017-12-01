db51a8419e3f13e92179d87ebdf4986d81f63570
Chris Bruno <Chris234k@gmail.com>
2017-11-30 21:42:20 -0500

    Notes in editor

---
+++ Assets/Scripts/Notepad.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Notepad : ScriptableObject
+{
+    [Multiline(50)]
+    public string notes;
+}
